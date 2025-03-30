b618ee09a66c8767f02ce4d82f96ac165d2e8cf1
hieudien14310 <tranhieu14310@gmail.com>
2025-03-30 22:51:14 +0700

    add toast

--- urdraw-workspace-frontend/app/layout.js
+++ urdraw-workspace-frontend/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster richColors position="top-right" />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
