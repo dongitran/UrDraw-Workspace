4a41eeb906c78dae7205133221c5a971dcb50799
hieudien14310 <tranhieu14310@gmail.com>
2025-03-30 22:20:00 +0700

    add errorLog handler

--- urdraw-workspace-backend-bun/src/index.ts
+++ urdraw-workspace-backend-bun/src/index.ts
@@ -5,13 +5,19 @@ import DrawingRoutes from "@route/Drawings.routes";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { customLogger } from "@middlewars/index";
-
+import ErrorLog from "@middlewars/ErrorLog";
+import MongoConfig from "./config/mongodb";
 const app = new Hono();
+
+MongoConfig.connectToMongoDB();
+
 app.use(cors());
 app.use(logger(customLogger));
 
 app.route("/drawings", DrawingRoutes);
 app.route("/collections", CollectionRoute);
 app.route("/shares", ShareRoute);
+
+app.onError(ErrorLog);
 
 export default app;
