2913ed1eff2bfa1c3f7a5b4b95d737daace3f575
hieudien14310 <tranhieu14310@gmail.com>
2025-03-30 20:22:14 +0700

    add log

--- urdraw-workspace-backend-bun/src/index.ts
+++ urdraw-workspace-backend-bun/src/index.ts
@@ -3,9 +3,12 @@ import CollectionRoute from "@route/Collection.routes";
 import ShareRoute from "@route/Share.routes";
 import DrawingRoutes from "@route/Drawings.routes";
 import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { customLogger } from "@middlewars/index";
 
 const app = new Hono();
 app.use(cors());
+app.use(logger(customLogger));
 
 app.route("/drawings", DrawingRoutes);
 app.route("/collections", CollectionRoute);
