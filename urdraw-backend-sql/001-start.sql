21581151445786d99853844a601d4239955e6b64
hieudien14310 <tranhieu14310@gmail.com>
2025-04-02 21:49:42 +0700

    store sql logs

---
+++ urdraw-backend-sql/001-start.sql
@@ -0,0 +1,15 @@
+CREATE TABLE workspaces
+(
+    id          varchar(255) UNIQUE NOT NULL,
+    name        varchar(255)        NOT NULL,
+    user_id     varchar(255)        NOT NULL,
+    description text                NULL,
+    params      jsonb               NULL,
+    created_at  timestamptz         NOT NULL,
+    created_by  varchar(255)        NOT NULL,
+    updated_at  timestamptz,
+    updated_by  varchar(255),
+    deleted_at  timestamptz,
+    deleted_by  varchar(255),
+    PRIMARY KEY (id)
+);
