13f31af96b29d0959c237683db1bd854e40ae54d
hieudien14310 <tranhieu14310@gmail.com>
2025-04-07 18:25:47 +0700

    done

--- urdraw-backend-sql/005-test05.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE hieudien_test05
-(
-    id          varchar(255) UNIQUE NOT NULL,
-    name        varchar(255)        NOT NULL,
-    user_id     varchar(255)        NOT NULL,
-
-    PRIMARY KEY (id)
-);
