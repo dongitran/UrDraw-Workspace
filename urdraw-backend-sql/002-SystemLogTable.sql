e7a0f37e776ffb55dcaf58975b46e519e51658fb
hieudien14310 <tranhieu14310@gmail.com>
2025-04-07 20:07:00 +0700

    update

---
+++ urdraw-backend-sql/002-SystemLogTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE system_logs
+(
+    id                 varchar(255) UNIQUE NOT NULL,
+    user_id            varchar(255)        NOT NULL,
+    type               varchar(255)        NOT NULL,
+    related_id         varchar(255)        NOT NULL,
+    description        text                NOT NULL,
+    params             jsonb               NULL,
+    created_at         timestamptz         NOT NULL,
+    created_by         varchar(255)        NOT NULL,
+    updated_at         timestamptz,
+    updated_by         varchar(255),
+    deleted_at         timestamptz,
+    deleted_by         varchar(255),
+    PRIMARY KEY (id)
+);
