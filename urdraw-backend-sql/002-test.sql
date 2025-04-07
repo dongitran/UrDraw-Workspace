e21b4f4227d8b21c31ae965e84c10ba07d2e3556
hieudien14310 <tranhieu14310@gmail.com>
2025-04-07 11:14:03 +0700

    update sql yml

--- urdraw-backend-sql/002-test.sql
+++ urdraw-backend-sql/002-test.sql
@@ -5,6 +5,7 @@ CREATE TABLE invitations
     user_id            varchar(255)        NOT NULL,
     description        text                NULL,
     share_with_user_id varchar(255)        NOT NULL,
+
     params             jsonb               NULL,
     created_at         timestamptz         NOT NULL,
     created_by         varchar(255)        NOT NULL,
