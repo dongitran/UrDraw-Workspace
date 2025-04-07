9430319be2a5f2d4e5f3effd152ae7f479b339e1
hieudien14310 <tranhieu14310@gmail.com>
2025-04-07 11:22:21 +0700

    update test-sql

--- urdraw-backend-sql/002-test.sql
+++ urdraw-backend-sql/002-test.sql
@@ -5,7 +5,6 @@ CREATE TABLE invitations
     user_id            varchar(255)        NOT NULL,
     description        text                NULL,
     share_with_user_id varchar(255)        NOT NULL,
-
     params             jsonb               NULL,
     created_at         timestamptz         NOT NULL,
     created_by         varchar(255)        NOT NULL,
