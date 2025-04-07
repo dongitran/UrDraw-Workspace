a697be73d63764a2854e4e76170e364c9d66e269
hieudien14310 <tranhieu14310@gmail.com>
2025-04-07 11:10:18 +0700

    format to test

--- urdraw-backend-sql/002-test.sql
+++ urdraw-backend-sql/002-test.sql
@@ -1,15 +1,16 @@
 CREATE TABLE invitations
 (
-    id          varchar(255) UNIQUE NOT NULL,
-    name        varchar(255)        NOT NULL,
-    user_id     varchar(255)        NOT NULL,
-    description text                NULL,
-    params      jsonb               NULL,
-    created_at  timestamptz         NOT NULL,
-    created_by  varchar(255)        NOT NULL,
-    updated_at  timestamptz,
-    updated_by  varchar(255),
-    deleted_at  timestamptz,
-    deleted_by  varchar(255),
+    id                 varchar(255) UNIQUE NOT NULL,
+    name               varchar(255)        NOT NULL,
+    user_id            varchar(255)        NOT NULL,
+    description        text                NULL,
+    share_with_user_id varchar(255)        NOT NULL,
+    params             jsonb               NULL,
+    created_at         timestamptz         NOT NULL,
+    created_by         varchar(255)        NOT NULL,
+    updated_at         timestamptz,
+    updated_by         varchar(255),
+    deleted_at         timestamptz,
+    deleted_by         varchar(255),
     PRIMARY KEY (id)
 );
